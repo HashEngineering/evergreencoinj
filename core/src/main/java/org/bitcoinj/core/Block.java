2982733e777e0ba5890dbbd40946008455b3496a
HashEngineering <hashengineeringsolutions@gmail.com>
2019-08-06 09:25:11 -0700

    Add X15 algorithm for block hashes

--- core/src/main/java/org/bitcoinj/core/Block.java
+++ core/src/main/java/org/bitcoinj/core/Block.java
@@ -273,7 +273,7 @@ public class Block extends Message {
         time = readUint32();
         difficultyTarget = readUint32();
         nonce = readUint32();
-        hash = Sha256Hash.wrapReversed(Sha256Hash.hashTwice(payload, offset, cursor - offset));
+        hash = Sha256Hash.wrapReversed(X15.x15Digest(payload, offset, cursor - offset));
         headerBytesValid = serializer.isParseRetainMode();
 
         // transactions
